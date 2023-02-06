d2731c85fa1ee8fcab57fe9e98d905f599f39727
DexterChapman <96884737+DexterChapman@users.noreply.github.com>
2023-02-06 06:33:17 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const citrus =["lime", "lemon"];
+// const fruits =["apple", "bannana", ...citrus];
+// console.log(fruits);
+
+// const fullName ={
+//   fName:"james",
+//   lName:"bond"
+// };
+
+// const user={
+//   ...fullName,
+//   id:1,
+//   user: "James0007"
+// }
+
+// console.log(user)
